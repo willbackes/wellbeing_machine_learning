a1f8306112f48e1d7c7535fde28a88db874197e9
willbackes <s19wback@uni-bonn.de>
2024-02-24 18:12:36 +0100

    Add task to unzip data.

---
+++ src/wellbeing_and_machine_learning/data_management/task_unzip.py
@@ -0,0 +1,22 @@
+from wellbeing_and_machine_learning.config import BLD, SRC
+
+DATA_NAMES = {
+    "hbrutto.dta",
+    "hgen.dta",
+    "hl.dta",
+    "pgen.dta",
+    "pl.dta",
+    "ppathl.dta",
+}
+
+PRODUCES = {name: BLD / "data" / "unzip" / name for name in DATA_NAMES}
+
+
+def task_unzip_data(
+    depends_on=SRC / "data" / "SOEP_data.zip",
+    produces=PRODUCES,
+):
+    import zipfile
+
+    with zipfile.ZipFile(depends_on, "r") as zip_ref:
+        zip_ref.extractall(BLD / "data" / "unzip")
