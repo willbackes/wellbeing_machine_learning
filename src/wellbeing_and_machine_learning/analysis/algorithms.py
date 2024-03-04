d5e258d9bee5e4d6992cd110703682fe5a0f6913
willbackes <s19wback@uni-bonn.de>
2024-03-04 19:12:43 +0100

    Add function to run the RandomForest algorithm.

--- src/wellbeing_and_machine_learning/analysis/algorithms.py
+++ src/wellbeing_and_machine_learning/analysis/algorithms.py
@@ -1,5 +1,7 @@
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Lasso, LinearRegression
-from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV, train_test_split
 
 
 def ols_regression_by_year(data):
@@ -35,5 +37,37 @@ def lasso_regression_by_year(data, alphas=None, cv=4):
         grid_search.fit(X, Y)
         best_lasso_model = grid_search.best_estimator_
         r_squared = best_lasso_model.score(X, Y)
+        res[year] = r_squared
+    return res
+
+
+def random_forest_regression_by_year(
+    data,
+    n_estimators=100,
+    max_depth=None,
+    random_state=42,
+):
+    res = {}
+    for year in data["syear"].unique():
+        yearly_data = data[data["syear"] == year]
+        X = yearly_data.drop(["lifesatisfaction"], axis=1)
+        Y = yearly_data["lifesatisfaction"]
+
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X,
+            Y,
+            test_size=0.2,
+            random_state=42,
+        )
+
+        rf_model = RandomForestRegressor(
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            random_state=random_state,
+        )
+        rf_model.fit(X, Y)
+        rf_model.fit(X_train, Y_train)
+        Y_pred = rf_model.predict(X_test)
+        r_squared = r2_score(Y_test, Y_pred)
         res[year] = r_squared
     return res
