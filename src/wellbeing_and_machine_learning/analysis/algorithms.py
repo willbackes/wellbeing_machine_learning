a989e8f191162c428e5b72d323192a6718cf0831
willbackes <s19wback@uni-bonn.de>
2024-03-04 19:07:46 +0100

    Add function to run the LASSO algorithm.

--- src/wellbeing_and_machine_learning/analysis/algorithms.py
+++ src/wellbeing_and_machine_learning/analysis/algorithms.py
@@ -1,4 +1,5 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso, LinearRegression
+from sklearn.model_selection import GridSearchCV
 
 
 def ols_regression_by_year(data):
@@ -7,7 +8,32 @@ def ols_regression_by_year(data):
         yearly_data = data[data["syear"] == year]
         X = yearly_data.drop(["lifesatisfaction"], axis=1)
         Y = yearly_data["lifesatisfaction"]
+
         ols_model = LinearRegression()
         ols_model.fit(X, Y)
         res[year] = ols_model.score(X, Y)
+    return res
+
+
+def lasso_regression_by_year(data, alphas=None, cv=4):
+    if alphas is None:
+        alphas = [0.001, 0.01, 0.1, 1, 10]
+    res = {}
+    for year in data["syear"].unique():
+        yearly_data = data[data["syear"] == year]
+        X = yearly_data.drop(["lifesatisfaction"], axis=1)
+        Y = yearly_data["lifesatisfaction"]
+
+        param_grid = {"alpha": alphas}
+        lasso_model = Lasso()
+        grid_search = GridSearchCV(
+            estimator=lasso_model,
+            param_grid=param_grid,
+            cv=cv,
+            scoring="neg_mean_squared_error",
+        )
+        grid_search.fit(X, Y)
+        best_lasso_model = grid_search.best_estimator_
+        r_squared = best_lasso_model.score(X, Y)
+        res[year] = r_squared
     return res
