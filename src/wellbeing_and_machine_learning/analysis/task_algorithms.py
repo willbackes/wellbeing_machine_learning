a6d2c16ae99819612450e0a75a2077a335d64a4d
willbackes <s19wback@uni-bonn.de>
2024-03-05 22:55:30 +0100

    Add task to evaluate algorithms.

--- src/wellbeing_and_machine_learning/analysis/task_algorithms.py
+++ src/wellbeing_and_machine_learning/analysis/task_algorithms.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pytask
 
 from wellbeing_and_machine_learning.analysis.algorithms import (
+    algo_performance_and_variable_importance,
     algo_performance_by_year,
 )
 from wellbeing_and_machine_learning.config import ALGORITHMS, BLD
@@ -17,3 +18,20 @@ for algorithm in ALGORITHMS:
         data = pd.read_pickle(depends_on)
         r_squared = algo_performance_by_year(data, algo)
         r_squared.to_pickle(produces)
+
+
+for algorithm in ALGORITHMS:
+
+    @pytask.task(id=algorithm)
+    def task_algo_performance_and_variable_importance(
+        depends_on=BLD / "data" / "clean_data_converted.pkl",
+        algo=algorithm,
+        r_squared=BLD / "analysis" / f"{algorithm}_r_squared.pkl",
+        variable_importance=BLD / "analysis" / f"{algorithm}_variable_importance.pkl",
+        predicted_data=BLD / "analysis" / f"{algorithm}_predicted_data.pkl",
+    ):
+        data = pd.read_pickle(depends_on)
+        reg_results = algo_performance_and_variable_importance(data, algo)
+        reg_results[0].to_pickle(r_squared)
+        reg_results[1].to_pickle(variable_importance)
+        reg_results[2].to_pickle(predicted_data)
