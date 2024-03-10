77d6f88280c28ad11fe5afaf134f2ddb85dabdd1
willbackes <s19wback@uni-bonn.de>
2024-03-10 22:10:42 +0100

    Add tests and error handling to algorithms module.

--- tests/analysis/test_algorithms.py
+++ tests/analysis/test_algorithms.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from wellbeing_and_machine_learning.analysis.algorithms import (
     _gradient_boosting_regression,
     _lasso_regression,
     _ols_regression,
     _random_forest_regression,
+    _variable_importance,
 )
 
 
@@ -56,8 +58,8 @@ def test_lasso_regression_full_output_length():
     X, Y = make_regression(n_samples=100, n_features=2, noise=0.1)
     X = pd.DataFrame(X, columns=["logincome", "age"])
     Y = pd.Series(Y)
-    result = _lasso_regression(X, Y, r_squared_only=False)
-    assert len(result) == expected
+    actual = _lasso_regression(X, Y, r_squared_only=False)
+    assert len(actual) == expected
 
 
 def test_lasso_regression_full_output_r_squared_df():
@@ -81,16 +83,16 @@ def test_lasso_regression_with_grid_search():
     X, Y = make_regression(n_samples=100, n_features=2, noise=0.1)
     X = pd.DataFrame(X, columns=["logincome", "age"])
     Y = pd.Series(Y)
-    result = _lasso_regression(X, Y, r_squared_only=True, use_grid_search=True)
-    assert 0 <= result <= 1
+    actual = _lasso_regression(X, Y, r_squared_only=True, use_grid_search=True)
+    assert 0 <= actual <= 1
 
 
 def test_lasso_regression_without_grid_search():
     X, Y = make_regression(n_samples=100, n_features=2, noise=0.1)
     X = pd.DataFrame(X, columns=["logincome", "age"])
     Y = pd.Series(Y)
-    result = _lasso_regression(X, Y, r_squared_only=True, use_grid_search=False)
-    assert 0 <= result <= 1
+    actual = _lasso_regression(X, Y, r_squared_only=True, use_grid_search=False)
+    assert 0 <= actual <= 1
 
 
 def test_random_forest_regression_r_squared_only():
@@ -104,7 +106,7 @@ def test_random_forest_regression_r_squared_only():
         random_state=42,
     )
 
-    result = _random_forest_regression(
+    actual = _random_forest_regression(
         X_train,
         X_test,
         Y_train,
@@ -112,8 +114,8 @@ def test_random_forest_regression_r_squared_only():
         r_squared_only=True,
     )
 
-    assert isinstance(result, float)
-    assert 0 <= result <= 1
+    assert isinstance(actual, float)
+    assert 0 <= actual <= 1
 
 
 def test_random_forest_regression_full_output_length():
@@ -127,14 +129,14 @@ def test_random_forest_regression_full_output_length():
         test_size=0.2,
         random_state=42,
     )
-    result = _random_forest_regression(
+    actual = _random_forest_regression(
         X_train,
         X_test,
         Y_train,
         Y_test,
         r_squared_only=False,
     )
-    assert len(result) == expected
+    assert len(actual) == expected
 
 
 def test_random_forest_regression_full_output_r_squared_df():
@@ -208,14 +210,14 @@ def test_gradient_boosting_regression_r_squared_only():
         test_size=0.2,
         random_state=42,
     )
-    result = _gradient_boosting_regression(
+    actual = _gradient_boosting_regression(
         X_train,
         X_test,
         Y_train,
         Y_test,
         r_squared_only=True,
     )
-    assert 0 <= result <= 1
+    assert 0 <= actual <= 1
 
 
 def test_gradient_boosting_regression_full_output_length():
@@ -298,3 +300,52 @@ def test_gradient_boosting_regression_full_output_prediction_df():
         r_squared_only=False,
     )
     assert set(prediction_df.columns) == expected
+
+
+def test_variable_importance_output_type():
+    X, Y = make_regression(n_samples=100, n_features=2, noise=0.1)
+    X = pd.DataFrame(X, columns=["logincome", "age"])
+    Y = pd.Series(Y)
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X,
+        Y,
+        test_size=0.2,
+        random_state=42,
+    )
+    model = LinearRegression().fit(X_train, Y_train)
+    actual = _variable_importance(model, X_test, Y_test)
+    assert isinstance(actual, pd.DataFrame)
+
+
+def test_variable_importance_output_columns():
+    expected = {"variable_name", "PI"}
+    X, Y = make_regression(n_samples=100, n_features=2, noise=0.1)
+    X = pd.DataFrame(X, columns=["logincome", "age"])
+    Y = pd.Series(Y)
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X,
+        Y,
+        test_size=0.2,
+        random_state=42,
+    )
+    model = LinearRegression().fit(X_train, Y_train)
+    actual = _variable_importance(model, X_test, Y_test)
+    assert set(actual.columns) == expected
+
+
+def test_variable_importance_output_values():
+    expected = 0
+    X, Y = make_regression(n_samples=100, n_features=2, noise=0.1)
+    X = pd.DataFrame(X, columns=["logincome", "age"])
+    Y = pd.Series(Y)
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X,
+        Y,
+        test_size=0.2,
+        random_state=42,
+    )
+
+    model = LinearRegression().fit(X_train, Y_train)
+    actual = _variable_importance(model, X_test, Y_test)
+
+    assert all(actual["PI"] >= expected)
